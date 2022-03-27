012bdc84581cb5934a410379e2d5f640e7b77c61
Fefaodessa <fefaodessa@gmail.com>
2022-03-27 14:59:31 +0300

    DZ 8 Test 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+    import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+    
+    const onPlay = function(data) {
+        const time = data.seconds
+        localStorage.setItem("videoplayer-current-time", JSON.stringify(time));
+        
+    };
+    player.on('timeupdate', throttle(onPlay, 1000));
+
+    const timeStopPleyr = localStorage.getItem("videoplayer-current-time");
+    const timeStop =JSON.parse(timeStopPleyr)
+
+    player.setCurrentTime(timeStop)
+    
